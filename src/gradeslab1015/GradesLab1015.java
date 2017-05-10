0562c80cb4384295fbd9e0fc01450e442581c433
Koby Samuel <ksamuel890@gmail.com>
2017-05-10 13:45:14 -0400

    blah

---
+++ src/gradeslab1015/GradesLab1015.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradeslab1015;
+
+/**
+ *
+ * @author kms080
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class GradesLab1015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List grades = new ArrayList();
+    }
+}
